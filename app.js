df69cb0c91900fa381d42ba9d7ddee5835ff4f8a
pheonixash266 <110310052+pheonixash266@users.noreply.github.com>
2023-03-21 03:12:24 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
 function individualRoom() {
-document.getElementById("information").style.display = "block";
-document.getElementById("information1").style.display = "none";
-document.getElementById("information2").style.display = "none";
+document.getElementById("information"), position = "top";
+document.getElementById("information1"), position = "bottom";
+document.getElementById("information2"), position = "bottom";
 document.getElementById("room1").style.display = "block";
 document.getElementById("room2").style.display = "block";
-document.getElementById("room4").style.display = "none";
-document.getElementById("room3").style.display = "none";
-document.getElementById("room6").style.display = "none";
-document.getElementById("room5").style.display = "none";
+document.getElementById("room4"), position = "bottom";
+document.getElementById("room3"), position = "bottom";
+document.getElementById("room6"), position = "bottom";
+document.getElementById("room5"), position = "bottom";
 }
 function suiteRoom() {
-	document.getElementById("information").style.display = "none";
+	document.getElementById("information"), position = "bottom";
 	document.getElementById("information1").style.display = "block";
-	document.getElementById("information2").style.display = "none";
-	document.getElementById("room1").style.display = "none";
-	document.getElementById("room2").style.display = "none";
+	document.getElementById("information2"), position = "bottom";
+	document.getElementById("room1"), position = "bottom";
+	document.getElementById("room2"), position = "bottom";
 	document.getElementById("room4").style.display = "block";
 	document.getElementById("room3").style.display = "block";
-	document.getElementById("room6").style.display = "none";
-	document.getElementById("room5").style.display = "none";
+	document.getElementById("room6"), position = "bottom";
+	document.getElementById("room5"), position = "bottom";
 }
 function familyRoom() {
 	document.getElementById("information").style.display = "none";
